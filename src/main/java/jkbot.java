efeee4442185a613d3788679901df2bd55031af9
Juink4i <141946737+Juink4i@users.noreply.github.com>
2025-08-21 12:41:09 +0800

    level 2

--- src/main/java/jkbot.java
+++ src/main/java/jkbot.java
@@ -10,23 +10,29 @@ public class jkbot {
         System.out.println(opening);
         Scanner scanner = new Scanner(System.in);
 
-        ArrayList memory = new ArrayList();
+        ArrayList<String> memory = new ArrayList<>();
 
         while (true) {
             System.out.print("input: ");
             String text = scanner.nextLine();
-            // include storing text into a list
-            memory.add(text);
 
             if (text.equalsIgnoreCase("bye")) {
                 break;
             }
 
             if (text.equalsIgnoreCase("list")) {
-                //display list
+                System.out.println(line);
+                for (int i = 0; i < memory.size(); i++) {
+                    int index = i + 1;
+                    String printText = memory.get(i);
+                    System.out.println(index + ". " + printText + "\n");
+                }
+                System.out.println(line);
+            } else {
+                // include storing text into a list
+                memory.add(text);
+                System.out.println(line + "added: " + text + "\n" + line);
             }
-
-            System.out.println(line + "added: " + text + "\n" + line);
         }
 
         System.out.println(closing);
